42502b603bb518bcb87710002cbe0ac593879416
jarvi <jarvi>
1999-03-17 18:23:07 +0000

    Applied build fixes from 1.2 tree.

--- src/SerialImp.c
+++ src/SerialImp.c
@@ -17,11 +17,11 @@
 |   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --------------------------------------------------------------------------*/
 #include "config.h"
-#include <StubPreamble.h>
 #include "gnu_io_NativePort.h"
 #include <time.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
