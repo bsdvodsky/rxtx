9a5aa756c323e0e5da07a45b842d39a45b1f5089
jarvi <jarvi>
2002-04-05 20:09:03 +0000

    version fix

--- src/gnu/io/RXTXVersion.java
+++ src/gnu/io/RXTXVersion.java
@@ -37,7 +37,7 @@ public class RXTXVersion
 	private static String Version;
 
 	static {
-		Version = "RXTX-2.1-1";
+		Version = "RXTX-2.0-1";
 	}
 	/**
 	*  static method to return the current version of RXTX
