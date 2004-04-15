3356e4c211bf210f7363092c9a5229515407cae8
jarvi <jarvi>
2004-04-15 07:11:15 +0000

    fix namespace

--- src/ParallelImp.h
+++ src/ParallelImp.h
@@ -71,11 +71,11 @@
 #define PAR_EV_BUFFER	2
 
 /* java exception class names */
-#define UNSUPPORTED_COMM_OPERATION "javax.comm/UnsupportedCommOperationException"
+#define UNSUPPORTED_COMM_OPERATION "javax/comm/UnsupportedCommOperationException"
 #define ARRAY_INDEX_OUT_OF_BOUNDS "java/lang/ArrayIndexOutOfBoundsException"
 #define OUT_OF_MEMORY "java/lang/OutOfMemoryError"
 #define IO_EXCEPTION "java/io/IOException"
-#define PORT_IN_USE_EXCEPTION "javax.comm/PortInUseException"
+#define PORT_IN_USE_EXCEPTION "javax/comm/PortInUseException"
 
 /*
 Flow Control defines inspired by reading how mgetty by Gert Doering does it
